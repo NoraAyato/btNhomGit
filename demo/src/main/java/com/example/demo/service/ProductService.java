de1057285aabb05889d95667b08e7470b0b81a82
NoraAyato <laithanhtrunghutech@gmail.com>
2025-12-11 08:45:47 +0700

    feat(add): implement add and get product service

---
+++ demo/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.IProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final IProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
